0112def1926f477f78655c6dc8ff509a4dee02a8
Hammad Tariq <hammadtariq@yahoo.co.uk>
2015-09-06 17:48:14 +0500

    Initial commit
    Initial commit of ionic kitchen sink example

---
+++ ionic-kitchen-sink/README.md
@@ -0,0 +1,5 @@
+# Develop your IoT app using Ionic Framework and Evothings Studio (Kitchen Sink Example)
+
+This example is to demonstrate the usage of Ionic Framework Tabs with Evothings Studio.
+
+You can follow my tutorial for detailed instructions.
---
+++ ionic-kitchen-sink/js/app.js
@@ -0,0 +1,75 @@
+// Ionic kitchen sink app
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('ionicApp', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  //stateProvider keeps track of related templates
+  //If you want to add more views into the app, this is the place to declare them
+  $stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/home.html",
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.facts', {
+      url: "/facts",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts.html"
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: "/facts2",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts2.html"
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: "/about",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/about.html"
+        }
+      }
+    })
+    .state('tabs.navstack', {
+      url: "/navstack",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/nav-stack.html"
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: "/contact",
+      views: {
+        'contact-tab': {
+          templateUrl: "templates/contact.html"
+        }
+      }
+    });
+
+
+   $urlRouterProvider.otherwise("/tab/home");
+
+})
+
+//the home controller
+.controller('HomeTabCtrl', function($scope) {
+  console.log('HomeTabCtrl');
+});
